e3a6be7be65191c77df1305906dc7415da174653
Falldmoon <47499238+Falldmoon@users.noreply.github.com>
2020-03-14 19:11:29 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -21,4 +21,34 @@ $(document).ready(function () {
       $(this).next().not(":visible").slideDown().prev().addClass("active");
   });
   $('.input-phone').mask('+7 (000) 00-00-000');
+  $(window).on('resize', function() {
+      if ($(window).width() < 1200) {
+        $(".services-row").slick({
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrows: true,
+          infinite: true,
+          centerMode: true,
+          variableWidth: true,
+          dots: true
+        });
+      }
+      else{
+        
+      }
+  });
+    if ($(window).width() < 1200) {
+      $(".services-row").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: true,
+        variableWidth: true,
+        infinite: true,
+        dots: true
+      });
+    }
+    else{
+      
+    }
 });
