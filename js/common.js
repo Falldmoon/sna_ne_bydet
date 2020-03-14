15abf2b92eaa3e61bf01163898bcbab75d98fae9
Falldmoon <47499238+Falldmoon@users.noreply.github.com>
2020-03-14 10:58:38 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -5,9 +5,15 @@ $(document).ready(function () {
   	arrows: true,
   	centerMode: true,
   	variableWidth: true,
+  	swipe: false,
   	prevArrow: $('.works_slider_arow_l'),
   	nextArrow: $('.works_slider_arow_r'),
+  	asNavFor: '.works-slider-main',
   })
-  
+  $('.works-slider-main').slick({
+  	slidesToShow: 1,
+  	slidesToScroll: 1,
+  	asNavFor: '.works-navslider',
+  })
 
 });
