86bf8b9204cc2d5a61da72022405280d7fac902b
Falldmoon <47499238+Falldmoon@users.noreply.github.com>
2020-03-15 17:16:30 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -10,6 +10,14 @@ $(document).ready(function () {
   	prevArrow: $('.works_slider_arow_l'),
   	nextArrow: $('.works_slider_arow_r'),
   	asNavFor: '.works-slider-main',
+    responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            asNavFor: '.works-slider-mobile',
+          }
+        }
+      ]
   })
   $('.works-slider-main').slick({
   	slidesToShow: 1,
@@ -17,6 +25,16 @@ $(document).ready(function () {
   	slidesToScroll: 1,
   	asNavFor: '.works-navslider',
   })
+  $(".services-row").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    dots: true,
+    swipe: false,
+    prevArrow: $('.team-arrows__item_l_t'),
+    nextArrow: $('.team-arrows__item_r_t'),
+  });
   $('.team-slider').slick({
       slidesToShow: 1,
       dots: true,
@@ -25,47 +43,20 @@ $(document).ready(function () {
       prevArrow: $('.team-arrows__item_l'),
       nextArrow: $('.team-arrows__item_r'),
   })
+  $('.works-slider-mobile').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      swipe: false,
+      asNavFor: '.works-navslider',
+  })
+  $('.slider-type').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+  })
   $(".accordeon dd").hide().prev().click(function() {
       $(this).parents(".accordeon").find("dd").not(this).slideUp().prev().removeClass("active");
       $(this).next().not(":visible").slideDown().prev().addClass("active");
   });
   $('.input-phone').mask('+7 (000) 00-00-000');
-  $('.works-slider-main__slide').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  });
-  $(window).on('resize', function() {
-      if ($(window).width() < 1200) {
-        $(".services-row").slick({
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          arrows: true,
-          infinite: true,
-          centerMode: true,
-          variableWidth: true,
-          dots: true
-        });
-        $('.works-slider-main__slide').slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        });
-      }
-      else{
-        
-      }
-  });
-    if ($(window).width() < 1200) {
-      $(".services-row").slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        centerMode: true,
-        variableWidth: true,
-        infinite: true,
-        dots: true
-      });
-    }
-    else{
-      
-    }
 });
